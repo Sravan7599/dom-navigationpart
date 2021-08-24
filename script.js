97030cae1eb4ebf45d27df60f421b01a658bbc9a
Sravan7599 <kcmreddykummitha@gmail.com>
2021-08-24 15:25:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,161 @@
+// Navigation
+// 1.children - array of elements
+// 2.childNodes - (array of all the Nodes - text + elements)
+// 3.firstChild - text node of that element
+// 4.lastChild - last text node of that element
+// 5.firstElementChild - first element
+// 6.lastElementChild - last element
+// 7.previousSibling - previous sibling next node
+// 8.nextSibling -next sibling next node
+// 9.previousElementSibling - previous sibling element
+// 10.nextElementSibling - next sibling element
+// 11.parentElement / parentNode - parent of an element 
+
+
+let parentEl = document.getElementById("parent");
+console.log(parentEl);
+
+let parentEl1 = document.getElementById("parent");
+console.log(parentEl1.firstChild);
+
+let parentEl2 = document.getElementById("parent");
+console.log(parentEl2.lastChild);
+
+let parentEl3 = document.getElementById("parent");
+console.log(parentEl3.children);
+
+let parentEl4 = document.getElementById("parent");
+console.log(parentEl4.childNodes);
+
+let parentEl5 = document.getElementById("parent");
+console.log(parentEl5.firstElementChild);
+
+let parentEl6 = document.getElementById("parent");
+console.log(parentEl6.lastElementChild);
+
+let parentEl7 = document.getElementById("parent");
+console.log(parentEl7.children[3]);
+
+let midpara = document.getElementById("para");
+console.log(midpara);
+
+let midEl = document.getElementById("para");
+console.log(midEl.nextElementSibling);
+
+let midEl1 = document.getElementById("para");
+console.log(midEl1.previousElementSibling);
+
+let midEl2 = document.getElementById("para");
+console.log(midEl2.parentElement);
+
+
+
+
+
+
+
+
+
+// let parentEl = document.getElementById("parent");
+// console.log(parentEl.children);
+
+
+
+// let parentEl1 = document.getElementById("parent");
+// console.log(parentEl1.childNodes);
+
+// let parentEl2 = document.getElementById("parent");
+// console.log(parentEl2.firstChild);
+
+// let parentEl3 = document.getElementById("parent");
+// console.log(parentEl3.lastChild);
+
+// let parentEl4 = document.getElementById("parent");
+// console.log(parentEl4.firstElementChild);
+
+// let parentEl5 = document.getElementById("parent");
+// console.log(parentEl5.lastElementChild);
+
+// let parentEl6 = document.getElementById("parent");
+// console.log(parentEl6.previousSibling);
+
+// let parentEl7 = document.getElementById("parent");
+// console.log(parentEl7.nextSibling);
+
+// let parentEl8 = document.getElementById("parent");
+// console.log(parentEl8.previousElementSibling);
+
+// let parentEl9 = document.getElementById("parent");
+// console.log(parentEl9.nextElementSibling);
+
+// let parentele = document.getElementById("parent");
+// console.log(parentele.parentElement);
+
+
+
+
+
+// let content = document.getElementById("para");
+// console.log(content.childNodes);
+
+//  let parentE1=document.getElementById("parent")
+//  console.log(parentE1.children);
+
+//  let parentE2=document.getElementById("parent")
+//  console.log(parentE2.childNodes);
+
+//  let parentE3=document.getElementById("parent")
+//  console.log(parentE3.firstChild);
+
+//  let parentE4=document.getElementById("parent")
+//  console.log(parentE4.lastChild);
+
+//  let parentE5=document.getElementById("parent")
+//  console.log(parentE5.firstElementChild);
+
+//  let parentE6=document.getElementById("parent")
+//  console.log(parentE6.lastElementChild);
+
+//  let midE1=document.getElementById("midParagraph")
+//  console.log(midE1.previousSibling);
+
+//  let midE2=document.getElementById("midParagraph")
+//  console.log(midE2.nextSibling);
+
+//  let midE3=document.getElementById("midParagraph")
+//  console.log(midE3.previousElementSibling);
+
+//  let midE4=document.getElementById("midParagraph")
+//  console.log(midE4.nextElementSibling);
+
+
+
+
+
+
+
+
+//  sdfgd
+// let parentEl = document.getElementById("parent")
+// console.log(parentEl.firstChild)
+
+// let parentEl1 = document.getElementById("parent")
+// console.log(parentEl1.lastChild)
+
+// let parentElem = document.getElementById("parent")
+// console.log(parentElem.children)
+
+// let txt = document.getElementById("parent")
+// console.log(txt.childNodes)
+
+// let parentEl2 = document.getElementById("parent")
+// console.log(parentEl2.children[2])
+
+// let childEl = document.getElementById("part1")
+// console.log(childEl.nextElementSibling)
+
+// let childEle = document.getElementById("part1")
+// console.log(childEle.previousElementSibling)
+
+// let element = document.getElementById("part1")
+// console.log(element.parentElement)
